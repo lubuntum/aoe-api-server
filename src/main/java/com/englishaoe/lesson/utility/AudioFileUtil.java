9f37c4f70b27079688835b4a59125af26ad8cdb1
lubuntum <lubuntum@gmail.com>
2024-10-27 06:14:03 +0700

    Утилита для сохранения аудио файлов

---
+++ src/main/java/com/englishaoe/lesson/utility/AudioFileUtil.java
@@ -0,0 +1,29 @@
+package com.englishaoe.lesson.utility;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Component
+public class AudioFileUtil {
+    @Value("${audio.folderDir}")
+    private String folderDir;
+
+    public String saveAudioFile(MultipartFile file) {
+        try{
+            File directory = new File(folderDir);
+            if (!directory.exists())
+                directory.mkdirs();
+
+            String filePath = folderDir + file.getOriginalFilename();
+            File destinationFile = new File(filePath);
+            file.transferTo(destinationFile);
+            return filePath;
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+}
