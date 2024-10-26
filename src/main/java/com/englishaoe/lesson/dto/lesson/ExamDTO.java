3add6fa737bee91bc31c321b9921395bc731128a
lubuntum <lubuntum@gmail.com>
2024-10-27 06:14:54 +0700

    DTO класс для отправки данных экзамена не всех

---
+++ src/main/java/com/englishaoe/lesson/dto/lesson/ExamDTO.java
@@ -0,0 +1,13 @@
+package com.englishaoe.lesson.dto.lesson;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamDTO {
+    private Long id;
+    private String examCompleteDate;
+}
