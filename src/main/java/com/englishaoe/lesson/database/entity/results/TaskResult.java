d613c765bfe974af0844dda80ddb3a646db5e6f5
lubuntum <lubuntum@gmail.com>
2024-11-15 14:57:38 +0700

    Создал новую entity TaskResultType для хранения того, чья эта была проверка экспресс или эксперта + заполнил описательную часть двух entity

--- src/main/java/com/englishaoe/lesson/database/entity/results/TaskResult.java
+++ src/main/java/com/englishaoe/lesson/database/entity/results/TaskResult.java
@@ -1,4 +1,30 @@
 package com.englishaoe.lesson.database.entity.results;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "task_result")
 public class TaskResult {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_task_id")
+    private Long userTaskId;
+    @Column(name = "expert_id")
+    private Long expertId;
+    @Column(name = "task_result_type_id")
+    private Long taskResultTypeId;
+    @Column(name = "result")
+    private String result;
+    @Column(name = "send_date")
+    private String sendDate;
+
 }
---
+++ src/main/java/com/englishaoe/lesson/database/entity/results/TaskResultType.java
@@ -0,0 +1,21 @@
+package com.englishaoe.lesson.database.entity.results;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "task_result_type")
+public class TaskResultType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "type")
+    private String type;
+}
