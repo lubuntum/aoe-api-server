ec13ac4d144e2f35a63e531c65e4482f554d087f
lubuntum <lubuntum@gmail.com>
2024-11-21 19:35:17 +0700

    Комментарии + endpoint для получения результатов по определенному заданию без экзамена

--- src/main/java/com/englishaoe/lesson/controllers/AccountController.java
+++ src/main/java/com/englishaoe/lesson/controllers/AccountController.java
@@ -1,10 +1,13 @@
 package com.englishaoe.lesson.controllers;
 
 import com.englishaoe.lesson.database.entity.Customer;
+import com.englishaoe.lesson.database.entity.results.CustomerTask;
 import com.englishaoe.lesson.database.entity.results.Exam;
+import com.englishaoe.lesson.database.entity.results.TaskResult;
 import com.englishaoe.lesson.database.entity.variants.Variant;
 import com.englishaoe.lesson.database.repository.CustomerRepository;
 import com.englishaoe.lesson.database.services.CustomerServices;
+import com.englishaoe.lesson.database.services.CustomerTaskService;
 import com.englishaoe.lesson.database.services.ExamService;
 import com.englishaoe.lesson.database.services.VariantService;
 import com.englishaoe.lesson.dto.account.CustomerAccountDTO;
@@ -31,6 +34,8 @@ public class AccountController {
     @Autowired
     CustomerServices customerServices;
     @Autowired
+    CustomerTaskService customerTaskService;
+    @Autowired
     ExamService examService;
     //TODO add some method or filter for some routes which requier authentification like /customer, /header
     @Autowired
@@ -56,6 +61,7 @@ public class AccountController {
         CustomerAccountDTO customerAccountDTO = customerServices.getCustomerAccountDataById(Long.valueOf(jwtUtil.extractSubject(token)));
         return ResponseEntity.ok(customerAccountDTO);
     }
+    /** Just get all variants which customer completed (for result panel)*/
     @GetMapping("/customer/completed-variants")
     public ResponseEntity<List<Variant>> getCustomerCompletedVariants(@RequestHeader("Authorization") String token) {
         List<Exam> exams = examService.getExamsByCustomerId(Long.valueOf(jwtUtil.extractSubject(token)));
@@ -64,11 +70,19 @@ public class AccountController {
 
         return ResponseEntity.ok(variants);
     }
+    /**Get all exams for customer completed by some specific variant*/
     @GetMapping("/customer/exams-by-variant")
     public ResponseEntity<List<Exam>> getExamsCompletedByVariant(@RequestHeader("Authorization") String token,
                                                                  @RequestParam("variantId") Long variantId){
         List<Exam> exams = examService.getExamsByCustomerIdAndVariantId(
                 Long.valueOf(jwtUtil.extractSubject(token)), variantId);
         return ResponseEntity.ok(exams);
+    }
+    /** get all customerTask completed for the task out of exam (just one Task)*/
+    @GetMapping("/customer/task-by-variant")
+    public ResponseEntity<List<CustomerTask>> getTaskResultCompletedByTask(@RequestHeader("Authorization") String token,
+                                                                           @RequestParam("taskId") Long taskId){
+        List<CustomerTask> customerTasks = customerTaskService.getCustomerTaskOutOfExam(taskId, Long.valueOf(jwtUtil.extractSubject(token)));
+        return ResponseEntity.ok(customerTasks);
     }
 }
