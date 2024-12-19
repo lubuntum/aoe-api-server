bfa52c0aca7c4d07994201103a1afd2c9bacf6ca
lubuntum <lubuntum@gmail.com>
2024-12-20 06:32:57 +0700

    теперь функция определяет статический путь из полного пути без посредников

--- src/main/java/com/englishaoe/lesson/utility/file/FileUtil.java
+++ src/main/java/com/englishaoe/lesson/utility/file/FileUtil.java
@@ -5,8 +5,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class FileUtil {
-    public static String saveFileToDir(MultipartFile file, String folderPath, String staticPath){
+    public static String saveFileToDir(MultipartFile file, String folderPath){
         try{
             File directory = new File(folderPath);
             if (!directory.exists())
@@ -14,9 +17,13 @@ public class FileUtil {
             String filePath = folderPath + file.getOriginalFilename();
             File destinationFile = new File(filePath);
             file.transferTo(destinationFile);
-            return staticPath + file.getOriginalFilename();
+            return getStaticPath(folderPath) + file.getOriginalFilename();
         } catch (IOException e) {
             return e.getMessage();
         }
+    }
+    private static String getStaticPath(String absolutePath){
+        String path = Paths.get(absolutePath).getFileName().toString();
+        return (!path.endsWith("/")) ? path+"/" : path;
     }
 }
