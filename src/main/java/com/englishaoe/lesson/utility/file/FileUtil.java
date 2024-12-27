8210f923b6fae881948fb384c5ae4ceb88807224
lubuntum <lubuntum@gmail.com>
2024-12-27 12:15:14 +0700

    Добавил вариативность в функцию сохранения файлов, добавление уникального префикса по требованию

--- src/main/java/com/englishaoe/lesson/utility/file/FileUtil.java
+++ src/main/java/com/englishaoe/lesson/utility/file/FileUtil.java
@@ -7,20 +7,38 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Random;
 
 public class FileUtil {
-    public static String saveFileToDir(MultipartFile file, String folderPath){
+    public static String saveFileToDir(MultipartFile file, String folderPath, boolean addPrefix){
         try{
             File directory = new File(folderPath);
             if (!directory.exists())
                 directory.mkdirs();
-            String filePath = folderPath + file.getOriginalFilename();
+            String filePath = folderPath + (addPrefix ? generateUniqueFilename(file.getOriginalFilename()) : file.getOriginalFilename());
             File destinationFile = new File(filePath);
             file.transferTo(destinationFile);
             return getStaticPath(folderPath) + file.getOriginalFilename();
         } catch (IOException e) {
             return e.getMessage();
         }
+    }
+    private static String generateUniquePrefix() {
+        long currentTimeMillis = System.currentTimeMillis();
+        //int randomNumber = new Random().nextInt(1000);
+        return String.valueOf(currentTimeMillis);
+    }
+    private static String generateUniqueFilename(String filename) {
+        String prefix = generateUniquePrefix();
+        String baseName = filename.substring(0, filename.lastIndexOf("."));
+        int lastDotIndex = filename.lastIndexOf(".");
+
+        if (lastDotIndex == -1) {
+            return filename + "_" + prefix;
+        }
+
+        String fileExtension = filename.substring(filename.lastIndexOf("."));
+        return baseName + "_" + prefix + fileExtension;
     }
     private static String getStaticPath(String absolutePath){
         String path = Paths.get(absolutePath).getFileName().toString();
