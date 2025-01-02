a40589429803e90ee32c08c148ace2338c49915f
lubuntum <lubuntum@gmail.com>
2025-01-03 00:21:26 +0700

    Работа с Типами заданий, их запрос и обновление

--- src/main/java/com/englishaoe/lesson/database/repository/TaskTypeRepository.java
+++ src/main/java/com/englishaoe/lesson/database/repository/TaskTypeRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface TaskTypeRepository extends JpaRepository<TaskType, Long> {
     @Query("SELECT t.prompt FROM TaskType t WHERE t.type = :type")
     String findPromptByType(@Param("type") int type);
+    @Query("SELECT t FROM TaskType t where t.prompt IS NOT NULL")
+    List<TaskType> findAllTaskType();
     TaskType findByType(Integer type);
 }
--- src/main/java/com/englishaoe/lesson/database/services/TaskTypeService.java
+++ src/main/java/com/englishaoe/lesson/database/services/TaskTypeService.java
@@ -1,8 +1,13 @@
 package com.englishaoe.lesson.database.services;
 
+import com.englishaoe.lesson.database.entity.variants.TaskType;
 import com.englishaoe.lesson.database.repository.TaskTypeRepository;
+import com.englishaoe.lesson.exceptions.RegularException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskTypeService {
@@ -11,5 +16,15 @@ public class TaskTypeService {
 
     public String getPromptByType(int type) {
         return taskTypeRepository.findPromptByType(type);
+    }
+    public List<TaskType> getAllTaskType(){
+        return taskTypeRepository.findAllTaskType();
+    }
+    public TaskType updateTaskType(Long id, String prompt){
+        Optional<TaskType> taskTypeOptional = taskTypeRepository.findById(id);
+        if (taskTypeOptional.isEmpty()) throw new RegularException("TaskType not found", 404);
+        TaskType taskType = taskTypeOptional.get();
+        taskType.setPrompt(prompt);
+        return taskTypeRepository.save(taskType);
     }
 }
