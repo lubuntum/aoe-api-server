bde8b17dd2971fcc4cee2b2e01f0690a4040eeae
lubuntum <lubuntum@gmail.com>
2025-01-03 00:21:44 +0700

    Сделал проверку 1ого задания немного строже

--- src/main/java/com/englishaoe/lesson/textdistance/DistanceToGradeConverter.java
+++ src/main/java/com/englishaoe/lesson/textdistance/DistanceToGradeConverter.java
@@ -1,7 +1,7 @@
 package com.englishaoe.lesson.textdistance;
 
 public class DistanceToGradeConverter {
-    public static double LOWER_DISTANCE = 0.8;
+    public static double LOWER_DISTANCE = 0.9;
     public static int convert(double distance){
         return (distance >= LOWER_DISTANCE) ?  1 : 0;
     }
