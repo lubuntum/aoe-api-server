c56efe0c1e1fbff4f0ec374e10cf2d13e377acc8
lubuntum <lubuntum@gmail.com>
2025-01-03 00:22:28 +0700

    Вынес логику проверки админа в отдельный сервис (возмоно бизнеслогика регистрации и логина тоже перейдет сюда из контроллеров или в похожий сервис)

---
+++ src/main/java/com/englishaoe/lesson/services/AuthorizationService.java
@@ -0,0 +1,20 @@
+package com.englishaoe.lesson.services;
+
+import com.englishaoe.lesson.database.services.CustomerServices;
+import com.englishaoe.lesson.exceptions.RegularException;
+import com.englishaoe.lesson.utility.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationService {
+    @Autowired
+    JwtUtil jwtUtil;
+    @Autowired
+    CustomerServices customerServices;
+
+    public boolean isCustomerAdmin(String token) {
+        return customerServices.isCustomerHasAdminRole(Long.valueOf(jwtUtil.extractSubject(token)));
+    }
+}
