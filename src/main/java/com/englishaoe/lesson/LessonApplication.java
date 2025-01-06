dc81e6d4c57645a4016615ae79c867f73e1502f7
lubuntum <lubuntum@gmail.com>
2025-01-07 06:46:14 +0700

    Разрещил работу с временными сервисами

--- src/main/java/com/englishaoe/lesson/LessonApplication.java
+++ src/main/java/com/englishaoe/lesson/LessonApplication.java
@@ -2,8 +2,10 @@ package com.englishaoe.lesson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class LessonApplication {
 
 	public static void main(String[] args) {
