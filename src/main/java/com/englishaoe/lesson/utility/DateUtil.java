15db0c1558b579d15931b9bb604c0fa43b359ae3
lubuntum <lubuntum@gmail.com>
2025-01-20 19:27:59 +0700

    Метод для получения даты окончания подписки

--- src/main/java/com/englishaoe/lesson/utility/DateUtil.java
+++ src/main/java/com/englishaoe/lesson/utility/DateUtil.java
@@ -6,9 +6,14 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
+    public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
     public static String getCurrentDate() {
         LocalDate localDate = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         return formatter.format(localDate);
+    }
+    public static String getExpireDate(int months){
+        LocalDate localDate = LocalDate.now();
+        LocalDate expireSubscriptionDate = localDate.plusDays(months* 30L);
+        return formatter.format(expireSubscriptionDate);
     }
 }
