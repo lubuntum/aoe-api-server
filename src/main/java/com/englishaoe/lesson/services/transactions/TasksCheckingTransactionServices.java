e91fd987a883aff41aeca68554d94796fbc0f8f4
lubuntum <lubuntum@gmail.com>
2025-02-02 00:11:01 +0700

    метод для возврата денег для экзамена

--- src/main/java/com/englishaoe/lesson/services/transactions/TasksCheckingTransactionServices.java
+++ src/main/java/com/englishaoe/lesson/services/transactions/TasksCheckingTransactionServices.java
@@ -1,24 +1,36 @@
 package com.englishaoe.lesson.services.transactions;
 
+import com.englishaoe.lesson.database.entity.results.CheckStatusEnum;
+import com.englishaoe.lesson.database.entity.results.CustomerTask;
 import com.englishaoe.lesson.database.services.CustomerServices;
+import com.englishaoe.lesson.database.services.CustomerTaskService;
 import com.englishaoe.lesson.database.services.TaskTypeService;
 import com.englishaoe.lesson.dto.results.CustomerTaskDTO;
+import com.englishaoe.lesson.taskcheck.TaskCheckEndHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
+
 @Service
 public class TasksCheckingTransactionServices {
     @Autowired
     private TaskTypeService taskTypeService;
     @Autowired
     private CustomerServices customerServices;
+    @Autowired
+    private CustomerTaskService customerTaskService;
 
     public Boolean payForChecking(Long customerId, BigDecimal price){
         return customerServices.subBalanceToCustomerById(customerId, price);
     }
     public Boolean refund(Long customerId, BigDecimal price) {
         return customerServices.addBalanceToCustomerById(customerId, price);
+    }
+    public void refundFailedExamForSuccessTasks(Long customerId){
+        BigDecimal examPrice = taskTypeService.getTotalPrice();
+        refund(customerId, examPrice);
     }
 
 }
