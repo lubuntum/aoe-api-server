924a955e6d2056dc4b58d77c342ceeac1a3176e5
lubuntum <lubuntum@gmail.com>
2025-02-15 10:21:16 +0700

    endpoint для проверки подписки пользователя

--- src/main/java/com/englishaoe/lesson/controllers/SubscriptionController.java
+++ src/main/java/com/englishaoe/lesson/controllers/SubscriptionController.java
@@ -1,10 +1,12 @@
 package com.englishaoe.lesson.controllers;
 
 import com.englishaoe.lesson.database.entity.transactions.SubscriptionType;
+import com.englishaoe.lesson.database.services.CustomerServices;
 import com.englishaoe.lesson.database.services.SubscriptionTransactionService;
 import com.englishaoe.lesson.database.services.SubscriptionTypeService;
 import com.englishaoe.lesson.utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,8 @@ public class SubscriptionController {
     SubscriptionTypeService subscriptionTypeService;
     @Autowired
     SubscriptionTransactionService subscriptionTransactionService;
+    @Autowired
+    CustomerServices customerServices;
     @GetMapping("/valid-subscriptions")
     public ResponseEntity<List<SubscriptionType>> validSubscriptions(){
         return ResponseEntity.ok(subscriptionTypeService.getAllValidSubscriptionType());
@@ -29,5 +33,14 @@ public class SubscriptionController {
         return ResponseEntity.ok(
                 subscriptionTransactionService.purchaseSubscriptionForCustomer(
                     Long.valueOf(jwtUtil.extractSubject(token)),subscriptionTypeId));
+    }
+    @GetMapping("/check-subscription")
+    public ResponseEntity<Boolean> checkSubscription(@RequestHeader("Authorization") String token) {
+        try {
+            return ResponseEntity.ok(
+                    customerServices.isCustomerSubscribed(Long.valueOf(jwtUtil.extractSubject(token))));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(false);
+        }
     }
 }
